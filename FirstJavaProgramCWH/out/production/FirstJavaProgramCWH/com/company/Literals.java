3a4bb20e27ddf907ec25253a3777ea43d7e92e40
RinkuDas01 <99033010+RinkuDas01@users.noreply.github.com>
2022-02-06 17:57:22 +0530

    Add files via upload

---
+++ FirstJavaProgramCWH/out/production/FirstJavaProgramCWH/com/company/Literals.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Literals {
+    public static void main(String[] args) {
+        // write your code here
+
+        byte age= 21;
+        System.out.println(age);
+        int age2= 34;
+        short age3= 10;
+        long ageDino= 39876543200l;
+        char ch= 'A';
+        float f1= 10.6f;
+        double d1= 43.76d;
+        boolean a= true;
+        String str= "Rinku";
+        System.out.println(str);
+
+
+
+    }
+}
