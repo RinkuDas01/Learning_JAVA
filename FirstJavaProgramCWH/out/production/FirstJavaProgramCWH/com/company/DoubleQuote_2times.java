ebfc2482103f0c4077f4bdc60ff824d5461e0961
RinkuDas01 <99033010+RinkuDas01@users.noreply.github.com>
2022-02-07 19:03:40 +0530

    Add files via upload

---
+++ FirstJavaProgramCWH/out/production/FirstJavaProgramCWH/com/company/DoubleQuote_2times.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class DoubleQuote_2times {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World! \" Welcome to JAVA");//put a back slash
+
+    }
+}
