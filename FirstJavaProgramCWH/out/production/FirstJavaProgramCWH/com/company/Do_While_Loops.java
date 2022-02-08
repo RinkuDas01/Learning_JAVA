127828133807b281aa4263f006b8c74ee6de811a
RinkuDas01 <99033010+RinkuDas01@users.noreply.github.com>
2022-02-08 18:07:09 +0530

    Add files via upload

---
+++ FirstJavaProgramCWH/out/production/FirstJavaProgramCWH/com/company/Do_While_Loops.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Do_While_Loops {
+    public static void main(String[] args) {
+//        int a=1;
+//        while(a<5){
+//            System.out.println(a);
+//            a++;
+//        }
+//        int b=0;
+//        do {
+//            System.out.println(b);
+//            b++;
+//        }while(b<5);
+//    }
+        int c=1;
+        do{
+            System.out.println(c);
+            c++;}while(c<=45);
+        }
+}
