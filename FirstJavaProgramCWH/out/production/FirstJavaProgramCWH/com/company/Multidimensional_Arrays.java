6e04da43ce6b500b647d3002cd10e2a30ff6fca5
RinkuDas01 <99033010+RinkuDas01@users.noreply.github.com>
2022-02-10 09:50:43 +0530

    Add files via upload

---
+++ FirstJavaProgramCWH/out/production/FirstJavaProgramCWH/com/company/Multidimensional_Arrays.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Multidimensional_Arrays {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [] marks; //1-D Arrays
+        int [][] flats; //2-D Arrays
+        flats=new int[2][3];
+        flats[0][0]=101;
+        flats[0][1]=102;
+        flats[0][2]=103;
+        flats[1][0]=201;
+        flats[1][1]=202;
+        flats[1][2]=203;
+        //Displaying the array
+        System.out.println("Printing using for loop:");
+        for(int i=0;i<flats.length;i++){
+            for(int j=0;j<flats[i].length;j++){
+            System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
