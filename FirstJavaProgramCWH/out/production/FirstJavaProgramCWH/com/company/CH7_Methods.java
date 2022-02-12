45fb1392692c41cabcf8a3d3ea8a641c3aeffb19
RinkuDas01 <99033010+RinkuDas01@users.noreply.github.com>
2022-02-12 08:33:52 +0530

    Add files via upload

---
+++ FirstJavaProgramCWH/out/production/FirstJavaProgramCWH/com/company/CH7_Methods.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CH7_Methods {
+        static int logic(int x,int y){
+            int z;
+            if(x<y){
+                z=(x+y);
+            }
+            else{
+                z=(x+y)*5;
+            }
+            return z;
+        }
+        public static void main(String[] args){
+    int a=9;
+    int b=6;
+    int c;
+    c=logic(a,b);
+    int a1=9;
+    int b1=16;
+    int c1;
+    c1=logic(a1,b1);
+    System.out.println(c);
+    System.out.println(c1);
+
+
+
+}
+}
