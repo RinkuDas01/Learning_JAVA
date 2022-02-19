5accd2bb2dcb13b8928dc4cb53034bb96ad9fd3d
RinkuDas01 <99033010+RinkuDas01@users.noreply.github.com>
2022-02-19 18:45:49 +0530

    Add files via upload

---
+++ FirstJavaProgramCWH/out/production/FirstJavaProgramCWH/com/company/CH10_Inheritance.java
@@ -0,0 +1,38 @@
+package com.company;
+
+class Base{
+    int x;
+    public int  getX (){
+        return x;
+    }
+    public void setX(int x){
+        System.out.println("I'm a base.");
+        this.x=x;
+    }
+    public void printMe(){
+        System.out.println("I'm a constructor.");
+    }
+}
+class Derived extends Base{
+    int y;
+    public int getY(){
+    return y;
+}
+public void setY(int y){
+    this.y=y;
+}
+}
+public class CH10_Inheritance {
+    public static void main(String[] args) {
+        //creating an object of base class
+        Base b=new Base();
+        b.setX(5);
+        System.out.println(b.getX());
+        //creating an object of derived class
+        Derived d=new Derived();
+        d.setY(43);
+        System.out.println(d.getY());
+
+
+    }
+}
